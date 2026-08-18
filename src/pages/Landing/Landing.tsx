bcb8dfac0614f1b94184e5944ad188738cdf6e29
Masande07i <magoso.nondumo@gmail.com>
2026-08-18 15:29:38 +0200

    added the styling on login

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Text } from '../../components/Text/Text'
+import style from './Landing.module.css'
+import { FiShoppingBag } from "react-icons/fi";
+import { Button } from '../../components/Button/Button';
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
+    <section>
+    <div className={style.logo}>
+        <FiShoppingBag className={style.icon}/>
+        <Text variant='h1' style={{ color: '#FF4EA7'}} > Shopping List</Text>
+    </div>
+    <div >
+        <Button label = "LOGIN" type='button' />
+        <Button label = "SIGNUP" type='button' />
+    </div>
+    </section>
+   
   )
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { Text } from "../../components/Text/Text"
+import style from './Login.module.css'
+import { Button } from "../../components/Button/Button"
+import { FiShoppingBag } from "react-icons/fi";
 
 export const Login = () => {
   return (
-    <div>Login</div>
+   <section className= {style.slate}>
+        <div className ={style.logo}>
+         <FiShoppingBag className={style.icon}/>
+         <Text variant= "h1" style={{ color:' #FF4EA7'}}> Shopping List</Text>
+        </div>
+        <Text variant= "h2"> Welcome Back!!</Text>
+       
+   
+     <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter an email"
+         />
+     </div>
+     
+     <div>
+        <label htmlFor="text">Password:</label>
+        <input
+          type="password"
+          name="text"
+          placeholder="Enter your passwword"
+         />
+     </div>
+       <Button label = "LOGIN" type='button' />
+     </section>
   )
 }
